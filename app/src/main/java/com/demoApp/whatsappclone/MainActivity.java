ef005c3493797037cc5d48cd22b5463823848ffa
pareshsahoo902 <pareshsahoo902@gmail.com>
2021-06-26 21:52:28 +0530

    initial commit

---
+++ app/src/main/java/com/demoApp/whatsappclone/MainActivity.java
@@ -0,0 +1,33 @@
+package com.demoApp.whatsappclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },2000);
+
+
+    }
+}
